36950dc69b211c8188ccc0175caf7a9be4df2351
agent-indigo <bradenhynes@gmail.com>
2024-11-29 14:48:28 -0330

    Cleanup

--- serverActions/getSessionUserPrefs.ts
+++ serverActions/getSessionUserPrefs.ts
@@ -1,43 +1,29 @@
 'use server'
-import {getServerSession} from 'next-auth'
 import {Model} from 'sequelize'
-import authOpts from '@/config/authOpts'
 import PrefsSqlRecord from '@/interfaces/PrefsSqlRecord'
 import ServerActionResponse from '@/interfaces/ServerActionResponse'
-import SessionWithUserId from '@/interfaces/SessionWithUserId'
-import UserSqlRecord from '@/interfaces/UserSqlRecord'
 import prefsSqlModel from '@/models/prefsSqlModel'
-import userSqlModel from '@/models/userSqlModel'
-import connectSequelize from '@/utilities/connectSequelize'
+import getSessionUser from '@/serverActions/getSessionUser'
 const getSessionUserPrefs: Function = async (): Promise<ServerActionResponse> => {
   try {
-    const session: SessionWithUserId | null = await getServerSession(authOpts)
-    if (session) {
-      await connectSequelize()
-      const user: Model<UserSqlRecord> | null = await userSqlModel.findByPk(session.user.id)
-      if (user) {
-        const prefs: Model<PrefsSqlRecord> | null = await prefsSqlModel.findOne({
-          where: {
-            user: user.toJSON().id
-          },
-          attributes: {
-            exclude: [
-              'id'
-            ]
-          }
-        })
-        return prefs ? {
-          success: true,
-          sessionUserPrefs: prefs.toJSON()
-        } : {
-          error: '500: Internal Server Error',
-          success: false
+    const {success, sessionUser}: ServerActionResponse = await getSessionUser()
+    if (success && sessionUser) {
+      const sessionUserPrefs: Model<PrefsSqlRecord> | null = await prefsSqlModel.findOne({
+        where: {
+          user: sessionUser.id
+        },
+        attributes: {
+          exclude: [
+            'id'
+          ]
         }
-      } else {
-        return {
-          error: '401: Unauthorized',
-          success: false
-        }
+      })
+      return sessionUserPrefs ? {
+        success: true,
+        sessionUserPrefs: sessionUserPrefs.toJSON()
+      } : {
+        error: '500: Internal Server Error',
+        success: false
       }
     } else {
       return {
