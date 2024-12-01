07dbbd1f81ecd9775627c877433e662fbb63ab4d
agent-indigo <bradenhynes@gmail.com>
2024-12-01 12:44:12 -0330

    Cleanup

--- config/sequelize.ts
+++ config/sequelize.ts
@@ -1,10 +1,10 @@
 import {Sequelize} from 'sequelize'
 const sequelize: Sequelize = new Sequelize({
-  database: process.env.PG_SQL_DB_NAME ?? 'nomad',
-  username: process.env.PG_SQL_DB_USER ?? 'postgres',
-  password: process.env.PG_SQL_DB_PW ?? '',
-  host: process.env.PG_SQL_DB_HOST ?? 'localhost',
-  port: parseInt(process.env.PG_SQL_DB_PORT ?? '5432'),
+  database: process.env.SQL_DB_NAME ?? 'nomad',
+  username: process.env.SQL_DB_USER ?? 'postgres',
+  password: process.env.SQL_DB_PW ?? '',
+  host: process.env.SQL_DB_HOST ?? 'localhost',
+  port: parseInt(process.env.SQL_DB_PORT ?? '5432'),
   dialect: 'postgres',
   dialectOptions: {
     ssl: {
