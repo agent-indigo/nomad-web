c0a661d5a2f4579d992db854303d7bea7abcefa5
agent-indigo <bradenhynes@gmail.com>
2024-12-04 13:54:44 -0330

    Added OAuth models and migrations

---
+++ migrations/CreateOAuthAccessToken.ts
@@ -0,0 +1,54 @@
+import {
+  DataTypes,
+  Model,
+  QueryInterface
+} from 'sequelize'
+import OAuthAccessTokenSqlRecord from '@/interfaces/OAuthAccessTokenSqlRecord'
+import createId from '@/utilities/createId'
+import createTimeStamps from '@/utilities/createTimeStamps'
+export const up: Function = async (queryInterface: QueryInterface): Promise<void> => await queryInterface.createTable<Model<OAuthAccessTokenSqlRecord>>(
+  'oauth_access_tokens', {
+    ...createId(),
+    application_id: {
+      type: DataTypes.TEXT,
+      allowNull: true,
+      references: {
+        model: 'OAuthApplication',
+        key: 'id'
+      }
+    },
+    resource_owner_id: {
+      type: DataTypes.TEXT,
+      allowNull: true,
+      references: {
+        model: 'User',
+        key: 'id'
+      }
+    },
+    token: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+      unique: true
+    },
+    refresh_token: {
+      type: DataTypes.TEXT,
+      allowNull: true,
+      unique: true
+    },
+    expires_in: {
+      type: DataTypes.NUMBER,
+      allowNull: true,
+      defaultValue: 0
+    },
+    revoked_at: {
+      type: DataTypes.DATE,
+      allowNull: true
+    },
+    scopes: {
+      type: DataTypes.TEXT,
+      allowNull: true
+    },
+    ...createTimeStamps()
+  }
+)
+export const down: Function = async (queryInterface: QueryInterface): Promise<void> => await queryInterface.dropTable('oauth_access_tokens')
---
+++ migrations/CreateOAuthApplication.ts
@@ -0,0 +1,68 @@
+import {
+  DataTypes,
+  Model,
+  QueryInterface
+} from 'sequelize'
+import OAuthApplicationSqlRecord from '@/interfaces/OAuthApplicationSqlRecord'
+import createId from '@/utilities/createId'
+import createTimeStamps from '@/utilities/createTimeStamps'
+export const up: Function = async (queryInterface: QueryInterface): Promise<void> => await queryInterface.createTable<Model<OAuthApplicationSqlRecord>>(
+  'oauth_applications', {
+    ...createId(),
+    owner_id: {
+      type: DataTypes.TEXT,
+      allowNull: true,
+      references: {
+        model: 'User',
+        key: 'id'
+      }
+    },
+    uid: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+      unique: true
+    },
+    owner_type: {
+      type: DataTypes.TEXT,
+      allowNull: true
+    },
+    name: {
+      type: DataTypes.TEXT,
+      allowNull: false
+    },
+    secret: {
+      type: DataTypes.TEXT,
+      allowNull: false
+    },
+    redirect_url: {
+      type: DataTypes.TEXT,
+      allowNull: true,
+      validate: {
+        isUrl: true
+      }
+    },
+    scopes: {
+      type: DataTypes.TEXT,
+      allowNull: false
+    },
+    superapp: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: false
+    },
+    confidential: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: false
+    },
+    website: {
+      type: DataTypes.TEXT,
+      allowNull: true,
+      validate: {
+        isUrl: true
+      }
+    },
+    ...createTimeStamps()
+  }
+)
+export const down: Function = async (queryInterface: QueryInterface): Promise<void> => await queryInterface.dropTable('oauth_applications')
