844173e4f723b5f2e1e4c09270ce66f8d2cc7752
agent-indigo <bradenhynes@gmail.com>
2024-12-18 22:05:33 -0330

    Corrected static files path

--- app/settings.py
+++ app/settings.py
@@ -117,11 +117,11 @@ STATIC_ROOT = os.path.join(
   BASE_DIR,
   'static'
 )
-STATIC_URL = 'static/'
+STATIC_URL = 'static'
 STATICFILES_DIRS = [
   os.path.join(
     BASE_DIR,
-    'btre/static'
+    'app/static'
   )
 ]
 # Default primary key field type
