69468992a3ddc4176982f97633fc23d0d982debb
agent-indigo <bradenhynes@gmail.com>
2024-12-19 22:34:24 -0330

    Cleanup

--- app/settings.py
+++ app/settings.py
@@ -48,24 +48,24 @@ MIDDLEWARE = [
   'django.middleware.clickjacking.XFrameOptionsMiddleware'
 ]
 ROOT_URLCONF = 'app.urls'
-TEMPLATES = [
-  {
-    'BACKEND': 'django.template.backends.django.DjangoTemplates',
-    'DIRS': [os.path.join(
+TEMPLATES = [{
+  'BACKEND': 'django.template.backends.django.DjangoTemplates',
+  'DIRS': [
+    os.path.join(
       BASE_DIR,
       'app/templates'
-    )],
-    'APP_DIRS': True,
-    'OPTIONS': {
-      'context_processors': [
-        'django.template.context_processors.debug',
-        'django.template.context_processors.request',
-        'django.contrib.auth.context_processors.auth',
-        'django.contrib.messages.context_processors.messages'
-      ]
-    }
+    )
+  ],
+  'APP_DIRS': True,
+  'OPTIONS': {
+    'context_processors': [
+      'django.template.context_processors.debug',
+      'django.template.context_processors.request',
+      'django.contrib.auth.context_processors.auth',
+      'django.contrib.messages.context_processors.messages'
+    ]
   }
-]
+}]
 WSGI_APPLICATION = 'app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
