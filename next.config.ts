0e3cf73897fbd8b08967e78a1fec1d40752dd076
agent-indigo <bradenhynes@gmail.com>
2025-04-30 14:33:37 -0230

    Added remaining next.config properties

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,5 @@
 import {NextConfig} from 'next'
+import {Rewrite} from 'next/dist/lib/load-custom-routes'
 const domain: string = process.env.VERCEL_URL ?? process.env.DOMAIN ?? 'localhost:3000'
 const url: string = `http${domain === 'localhost:3000' ? '' : 's'}://${domain}`
 const nextConfig: NextConfig = {
@@ -7,6 +8,34 @@ const nextConfig: NextConfig = {
     NEXT_PUBLIC_API_DOMAIN: `${url}/api`,
     NEXTAUTH_URL: url,
     NEXTAUTH_SECRET: process.env.AUTH_SECRET ?? 'd3v3l0pm3nt53cr3tk3yn0t53cur3@t@11n3v3ru53!npr0duct!0n3v3r!!!'
-  }
+  },
+  images: {
+    remotePatterns: [{
+      protocol: 'https',
+      hostname: '**'
+    }]
+  },
+  output: process.env.VERCEL_URL ? undefined : 'standalone',
+  reactStrictMode: true,
+  generateBuildId: (): string => `nomad-web-${Date.now()}`,
+  rewrites: async (): Promise<Rewrite[]> => [{
+    source: '/nomad/_next/:path*',
+    destination: '/_next/:path*'
+  }, {
+    source: '/.well-known/:path*',
+    destination: '/api/ap/well-known/:path*'
+  }, {
+    source: '/.wellknown/:path*',
+    destination: '/api/ap/well-known/:path*'
+  }, {
+    source: '/users/:path*',
+    destination: '/api/ap/users/:path*'
+  }, {
+    source: '/inbox',
+    destination: '/api/ap/inbox'
+  }, {
+    source: '/nodeinfo',
+    destination: '/api/ap/nodeinfo'
+  }]
 }
 export default nextConfig
