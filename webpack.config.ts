1c207e15b07385221936f17ab01f71b2875ce1d3
agent-indigo <bradenhynes@gmail.com>
2025-07-12 13:43:38 -0230

    Added support for `src/**/*.ts`

--- webpack.config.ts
+++ webpack.config.ts
@@ -7,12 +7,13 @@ const webpackConfig: Configuration = {
   resolve: {
     extensions: [
       '.js',
+      '.ts',
       '.tsx'
     ]
   },
   module: {
     rules: [{
-      test: /\.(js|tsx)$/,
+      test: /\.(js|ts|tsx)$/,
       exclude: /node_modules/,
       use: {
         loader: 'babel-loader'
