b713358ded12ad266518b76a516a438dfb0da46d
agent-indigo <bradenhynes@gmail.com>
2025-07-12 13:47:35 -0230

    Implemented PEP8 formatting in `manage.py`

--- manage.py
+++ manage.py
@@ -2,6 +2,7 @@
 """Django's command-line utility for administrative tasks."""
 import os
 from sys import argv
+from django.core.management import execute_from_command_line
 def main() -> None:
     """Run administrative tasks."""
     os.environ.setdefault(
@@ -9,13 +10,12 @@ def main() -> None:
         'nomad.settings'
     )
     try:
-        from django.core.management import execute_from_command_line
+        execute_from_command_line(argv)
     except ImportError as exc:
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
-    execute_from_command_line(argv)
 if __name__ == '__main__':
     main()
