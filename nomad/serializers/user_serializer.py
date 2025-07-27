3c90bc700f1e5960aeeb54884f9b1cfef3e04b15
agent-indigo <bradenhynes@gmail.com>
2025-07-27 16:47:20 -0230

    Added missing user serializer fields

--- nomad/serializers/user_serializer.py
+++ nomad/serializers/user_serializer.py
@@ -17,5 +17,7 @@ class UserSerializer(ModelSerializer):
             'username',
             'email',
             'first_name',
-            'last_name'
+            'last_name',
+            'is_superuser',
+            'is_staff'
         ]
